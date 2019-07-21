1ce6cf6e38509351d3563018bae0ea98b8d3a385
Karl Tryggvason <ktryggvason@gmail.com>
2019-07-21 19:50:06 +0200

    Use box plot for mix track counts

--- src/drawTrackCount.js
+++ src/drawTrackCount.js
@@ -1,31 +1,50 @@
 import {GoogleCharts} from "google-charts"
-import {createMixLink} from "./lib.js"
+import {completeTracklist} from "./lib.js"
 
 export function drawTrackCount(mixes){
 	let dataTable = new GoogleCharts.api.visualization.DataTable({
 		cols: [
-			{id: 'date', label: 'Date', type: 'string'},
-			{id: 'count', label: 'Count', type: 'number'},
+			{id: 'date', label: 'Year', type: 'string'},
+			{id: 'count', label: 'Min', type: 'number'},
+			{id: 'count', label: 'Lowest Q', type: 'number'},
+			{id: 'count', label: 'Highest Q', type: 'number'},
+			{id: 'count', label: 'Max', type: 'number'},
 			{type: 'string', role: 'tooltip', 'p': {'html': true}}
 		],
 	})
 
-	let total = 0
-	dataTable.addRows(mixes.reduce((acc, mix, index) => {
-		total += mix.tracklist.length
-		let runningAverage
-		if (index < 52){
-			runningAverage = total / index + 1
-		} else {
-			total = total - mixes[index - 52].tracklist.length
-			runningAverage = total / 52
+	const trackCountsByYear = mixes.reduce((acc, mix) => {
+		if (!mix.duplicate){
+			const year = mix.date.slice(0, 4)
+			acc[year] ? acc[year].push(mix.tracklist.length) : acc[year] = [mix.tracklist.length]
 		}
-		const row = [mix.date, mix.tracklist.length, createMixLink(mix)]
-		acc.push(row)
+		return acc
+	}, {})
+
+	dataTable.addRows(Object.keys(trackCountsByYear).reduce((acc, year) => {
+
+		const mixCount = trackCountsByYear[year].length
+		const average = Math.floor(trackCountsByYear[year].reduce((acc, elem) => acc + elem, 0) / mixCount)
+
+		const sorted = trackCountsByYear[year].sort((a, b) => a - b)
+	    const middle = Math.floor((mixCount - 1) / 2)
+		const median = mixCount % 2 ? sorted[middle] :  (sorted[middle] + sorted[middle + 1]) / 2.0
+
+		const max = Math.max(...trackCountsByYear[year])
+		const min = Math.min(...trackCountsByYear[year])
+
+
+		const lowestQuartile = sorted[Math.floor((mixCount-1) * 0.25)]
+		const highestQuartile = sorted[Math.floor((mixCount-1) * 0.75)]
+
+		let tooltip = `Number of mixes: ${trackCountsByYear[year].length} <br> `
+		tooltip += `Most tracks: ${max} <br>`
+		tooltip += `Fewest tracks : ${min} <br>`
+		tooltip += `Average track count: ${average}<br>`
+		tooltip += `Median track count: ${median}`
+		acc.push([year, min, lowestQuartile, highestQuartile, max, tooltip])
 		return acc
 	}, []))
-
-	console.log(dataTable);
 
 	dataTable.sort({column: 0, asc: true});
 
@@ -48,6 +67,6 @@ export function drawTrackCount(mixes){
         }
 	}
 
-	let chart = new GoogleCharts.api.visualization.LineChart(document.getElementById('track-count'));
+	let chart = new GoogleCharts.api.visualization.CandlestickChart(document.getElementById('track-count'));
 	chart.draw(dataTable, options);
 }
--- src/lib.js
+++ src/lib.js
@@ -1,3 +1,7 @@
 export function createMixLink(mix){
 	return `<a href="https://www.mixesdb.com${mix.url}" target="_blank">${mix.date} - ${mix.artists.join(', ')}</a>`
 }
+
+export function completeTracklist(mix){
+	return mix['categories'] && mix['categories'].indexOf('Tracklist: complete') !== -1
+}

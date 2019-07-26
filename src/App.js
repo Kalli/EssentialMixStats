55ad1cf1d7f277ca3fb367b61b9e84386fbcc2ea
Karl Tryggvason <ktryggvason@gmail.com>
2019-07-26 22:24:23 +0200

    Start moving app to react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React, { Component} from "react"
 
 class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: true
+        }
+    }
+
+    loadData() {
+        fetch(`/data/data.json`)
+            .then( (response) => {
+                return response.json()
+            })
+            .then( (json) => {
+            	this.setState({loading: false, data: json})
+            });
+    }
+
+    componentDidMount() {
+        this.setState({
+	        loading: true,
+            data: this.loadData()
+        })
+    }
+	render(){
+    	const loading = this.state.loading && <div>Data is loading</div>
+
+		return(
+			<>
+				<div className="row text-center main">
+					<h1 className="text-center">Essential Mix stats</h1>
+					<p className="lead">
+						Explore the statistics and history of the BBC Essential Mix.
+					</p>
+					{loading}
+				</div>
+			</>
+		)
+	}
 }
 
 export default App

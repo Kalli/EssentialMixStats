25b48eb24804efce11ac26d125218bfc3de65ce9
Karl Tryggvason <ktryggvason@gmail.com>
2019-08-01 21:41:23 +0200

    Add categories by year breakdown

--- src/MixCategories.js
+++ src/MixCategories.js
@@ -5,10 +5,20 @@ import {reduceCategories} from "./lib"
 
 export default class MixCategories extends Component {
 	constructor(props) {
-		super(props);
+		super(props)
+		const categoryCounts = this.getMixesByCategories(this.props.mixes)
+		const categories = categoryCounts.reduce((acc, e) => {
+			acc.push(e[0])
+			return acc
+		}, [])
+		this.state = {
+			categoryCounts: categoryCounts,
+			allCategories: categories,
+			selectedCategories: categories.slice(0, 3)
+		}
 	}
 
-	formatData(mixes){
+	getMixesByCategories(mixes){
 		const mixesByCategories = reduceCategories(mixes, ['Tracklist'])
 
 		return mixesByCategories.reduce((acc, category) => {
@@ -22,34 +32,122 @@ export default class MixCategories extends Component {
 		}, [])
 	}
 
-	render(){
-		const mixes = this.formatData(this.props.mixes)
+	getCategoriesByYears(mixes, categories){
+		const yearMap = [...Array(2019-1992).keys()].map((i)=>{return 2019-i}).reduce((acc, year) => {
+			const c = {}
+			categories.forEach((e)=>c[e] = 0)
+			acc[year] = c
+			return acc
+		}, {})
 
-		return 	<Chart
-			height={900}
-			className={"center-block"}
-			chartType="ColumnChart"
-			loader={<div>Loading Chart</div>}
-			data={[
-				[
-					{id: 'category', label: 'Category', type: 'string'},
-					{id: 'count', label: 'Count', type: 'number'},
-					{type: 'string', role: 'tooltip', 'p': {'html': true}},
-				],
-				...mixes
-			]}
-			options={{
-				title: 'Categories',
-				chartArea: { width: '80%' },
-				hAxis: {
-					title: 'Count',
-				},
-				vAxis: {
-					title: 'Categories',
-				},
-				tooltip: {isHtml: true, trigger: 'selection'},
-				legend: 'none',
-			}}
-		/>
+		const categoriesByYears = mixes.reduce((acc, mix) => {
+			const year = mix.date.slice(0, 4)
+			if (!mix.duplicate && year){
+				mix.categories.forEach((category) => {
+					if (categories.includes(category)){
+						acc[year][category] +=1
+					}
+				})
+			}
+			return acc
+		}, yearMap)
+		const header = [
+			{id: 'year', label: 'Year', type: 'string'},
+			...categories.map((cat) => ({id: cat, label: cat, type: 'number'}))
+		]
+
+		return Object.keys(categoriesByYears).reduce((acc, year) => {
+			acc.push([year, ...Object.entries(categoriesByYears[year]).map((e)=>e[1])])
+			return acc
+		}, [header])
+	}
+
+	handleSelectCategory(event){
+		const category = event.target.value
+		const index = this.state.selectedCategories.indexOf(category)
+		if (index === -1){
+			this.setState({selectedCategories: [...this.state.selectedCategories, category] })
+		}else{
+			if (this.state.selectedCategories.length > 1){
+				const selectedCategories = this.state.selectedCategories.filter((_, i) => i !== index)
+				this.setState({selectedCategories: selectedCategories })
+			}
+		}
+	}
+
+	selectCategories(categories){
+		return (
+			<div>
+				{categories.map((c) => {
+					return (
+						<label className={"checkbox-inline"} key={c}>
+							<input
+								type={"checkbox"}
+								checked={this.state.selectedCategories.includes(c)}
+								onChange={(value) => this.handleSelectCategory(value)}
+								value={c}
+							/>
+							{c}
+						</label>
+					)
+				})}
+			</div>
+		)
+	}
+
+	render(){
+		const categoriesByYear = this.getCategoriesByYears(this.props.mixes, this.state.selectedCategories)
+		return 	(
+			<>
+				<Chart
+					height={900}
+					className={"center-block"}
+					chartType="ColumnChart"
+					loader={<div>Loading Chart</div>}
+					data={[
+						[
+							{id: 'category', label: 'Category', type: 'string'},
+							{id: 'count', label: 'Count', type: 'number'},
+							{type: 'string', role: 'tooltip', 'p': {'html': true}},
+						],
+						...this.state.categoryCounts
+					]}
+					options={{
+						title: 'Categories',
+						chartArea: {'width': '80%', 'height': '80%'},
+						hAxis: {
+							title: 'Count',
+						},
+						vAxis: {
+							title: 'Categories',
+						},
+						tooltip: {isHtml: true, trigger: 'selection'},
+						legend: 'none',
+					}}
+				/>
+				{this.selectCategories(this.state.allCategories)}
+				<Chart
+					height={900}
+					className={"center-block"}
+					chartType="LineChart"
+					loader={<div>Loading Chart</div>}
+					data={categoriesByYear}
+					options={{
+						title: 'Categories by Year',
+						isStacked: 'relative',
+						chartArea: {'width': '80%', 'height': '80%'},
+						hAxis: {
+							title: 'Year',
+						},
+						vAxis: {
+							title: 'Count',
+							format: 0,
+						},
+						tooltip: {isHtml: true, trigger: 'selection'},
+						legend: 'bottom',
+					}}
+				/>
+			</>
+		)
 	}
 }
